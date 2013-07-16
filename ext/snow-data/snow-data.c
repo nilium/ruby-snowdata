0930912b0b09ed893466e4a17e3e52b681bf8f38
Noel Cower <ncower@gmail.com>
2013-07-16 02:40:21 -0700

    Update sd_memory_new (Memory::new) documentation.
    Notes that size is not optional, must be > 0. Also points out that it's
    aliased as wrap and __wrap__ and the rules for those.
    
    Updates default alignment note.

--- ext/snow-data/snow-data.c
+++ ext/snow-data/snow-data.c
@@ -1073,15 +1073,29 @@ static VALUE sd_set_string(int argc, VALUE *argv, VALUE self)
 
 /*
   call-seq:
-      new(address, size = 0, alignment = 1) => Memory
+      new(address, size, alignment = SIZEOF_VOID_POINTER) => Memory
 
-  Creates a new Memory object that wraps an existing pointer. Size and alignment
-  are optional and default to zero and 1, respectively.
+  Creates a new Memory object that wraps an existing pointer. Alignment is
+  optional and defaults to the size of a pointer (Memory::SIZEOF_VOID_POINTER).
 
-  Memory objects created with this method will not attempt to free the memory
-  they wrap, as they did not allocate it and so do not own it.
+  Size must be greater than zero. Zero-sized blocks are not permitted as they
+  render most memory functionality useless and make it very difficult to ensure
+  nothing bad is happening when you do bad things with snow-data. Because, let's
+  be honest with ourselves for a moment, everyone using this gem? They're bad
+  people. They're very bad people.
 
-  If a sub
+  Note that Memory objects created with this method will not attempt to free
+  the memory they wrap, as they did not allocate it and so do not own it. If
+  an address held by a Memory object is invalid, the Memory object is also
+  implicitly invalid as well, though there is no way for it to check this. You
+  are responsible for freeing any memory not allocated through ::malloc and
+  Memory subclasses.
+
+  It is an ArgumentError to provide a size of zero, nil, or false. It is also
+  an ArgumentError to provide a NULL (zero) address.
+
+  If a subclass overrides ::new, it is also aliased as ::wrap and ::__wrap__.
+  Subclasses may override ::wrap but must not override ::__wrap__.
  */
 static VALUE sd_memory_new(int argc, VALUE *argv, VALUE self)
 {
