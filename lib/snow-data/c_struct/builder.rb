9e59bf8d5aa84a9c546fe3331569b80fb0f122d4
Noel Cower <ncower@gmail.com>
2013-07-20 14:26:52 -0700

    Add CStruct::Builder class.
    This just acts as a way of building member info arrays by calling
    methods rather than building the arrays yourself or providing an
    encoding string. It's arguably easier to extend in the future than the
    encoding string stuff, and you'll get errors if you do something wrong.
    
    Future member declaration arguments will be added as keyword arguments,
    similar to the align keyword, thereby ensuring it's explicit and obvious
    what that part of the declaration does.

---
+++ lib/snow-data/c_struct/builder.rb
@@ -0,0 +1,70 @@
+require 'set'
+
+module Snow ; end
+class Snow::CStruct ; end
+
+class Snow::CStruct::Builder
+
+  @@defined_types = Set.new
+
+  def initialize(&block)
+    @members = []
+    @last_offset = 0
+    instance_exec(&block) if block_given?
+  end
+
+  def member_info
+    @members
+  end
+
+  def self.flush_type_methods!
+    ::Snow::CStruct::SIZES.each do |type_name, type_size|
+      next if @@defined_types.include?(type_name)
+
+      method_name = type_name.to_s
+      first_char = method_name[0]
+      case
+      when first_char >= ?A && first_char <= ?Z
+        method_name[0] = first_char.downcase
+      when first_char >= ?0 && first_char <= ?9
+        method_name[0] = "_#{first_char}"
+      end
+      method_name = method_name.to_sym
+
+      __send__(:define_method, method_name) do | name, lengths = 1, align: nil |
+        name = name.to_sym
+
+        length = case lengths
+                 when Integer, Fixnum then lengths
+                 when Array then lengths.reduce(:*)
+                 else lengths.to_i
+                 end
+        raise "Invalid length for member #{name}: must be >= 1" if length < 1
+
+        size = length * type_size
+
+        align = (align || ::Snow::CStruct::ALIGNMENTS[type_name]).to_i
+        raise "Nil alignment for type #{type_name}" if align.nil?
+
+        last_offset = instance_variable_get(:@last_offset)
+        offset = ::Snow::Memory.align_size(last_offset, align)
+        last_offset = offset + size
+
+        member_info = ::Snow::CStruct::StructMemberInfo[
+          name, type_name, size, length, align, offset]
+
+        instance_variable_set(:@last_offset, last_offset)
+        instance_variable_get(:@members).push(member_info.freeze)
+      end # define_method(type_name)
+
+      @@defined_types.add(type_name)
+
+    end # SIZES.each
+
+    ::Snow::CStruct::TYPE_ALIASES.each { |short, long|
+      __send__(:alias_method, short, long)
+    }
+
+  end # flush_type_methods!
+
+end
