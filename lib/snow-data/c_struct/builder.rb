33dd276e901082b47fb8eb31650835ebca49f0d5
Noel Cower <ncower@gmail.com>
2013-07-20 22:02:38 -0700

    Add unions/structs to the Builder class.
    This required a bit of fiddling with the Builder and how it assigns
    member offsets, so I'll try to go over that change here:
    
    - MemberStackLevel -- a Struct that's defined as a level of the CStruct.
      The root level is either a struct or union, but its offset is always
      zero. Level members may be either member info objects or descendant
      member levels. Descendant member levels are used in processing after
      members are declared. The order of members in a level is important for
      struct levels, though not unions, which is probably obvious.
    
    - After members are declared, the Builder calls adjust_level, which goes
      through all of a level's members and sets their offsets. Offsets at
      declaration are more or less not set, except to determine some initial
      and alignment attributes of levels. While it's iterating, it sets the
      size of each level based the difference of its base offset and ending
      offset. For unions, this is the maximum of differences and for structs
      it's just the last difference.
    
      When setting the offsets of members, it's important to keep in mind
      that the first member of a level and the level itself _may_ have
      differing alignments. This is almost always fine as long as the level
      has a greater than or equal alignment than its first member. If any
      member has a larger alignment than the level, those members may have
      padding bytes inserted before them, and those padding bytes may be
      used for other members with lesser alignments.
    
      In general, it's not advisable to manually set a level's alignment,
      though it can be done by passing a non-nil align: keyword argument to
      a level method (either union, struct, or __do_level__ if you're not
      thinking straight). By default, a level's alignment is the max of all
      the level's alignments, and this is often the best option for that
      level.

--- lib/snow-data/c_struct/builder.rb
+++ lib/snow-data/c_struct/builder.rb
@@ -7,9 +7,29 @@ require 'set'
 module Snow ; end
 class Snow::CStruct ; end
 
+#
+# Utility classes used by CStruct to create member info structs. Exposed to the
+# user via instance_exec blocks for declaring struct/union members.
+#
+# See CStruct::new, CStruct::struct, or CStruct::union.
+#
 class Snow::CStruct::Builder
 
+  #
+  # :nodoc:
+  #
+  # Struct describing a level of a C struct. Contains a flag for whether the
+  # level is a union, its offset, alignment, size, and its members (which may
+  # include descendant levels).
+  #
+  MemberStackLevel = Struct.new(:is_union, :offset, :alignment, :size, :members)
+
+
+  # :nodoc:
+  # A Set of symbols for all types whose declaration methods have already been
+  # defined by ::flush_type_methods!.
   @@defined_types = Set.new
+
 
   #
   # call-seq:
@@ -20,22 +40,72 @@ class Snow::CStruct::Builder
   # given, then it will be instance_exec'd, allowing you to easily call any
   # declaration methods on the builder instance.
   #
-  def initialize(&block)
-    @members = []
-    @last_offset = 0
+  def initialize(is_union: false, &block)
+    @member_names = Set.new
+    @level = MemberStackLevel[!!is_union, 0, 1, 0, []]
     instance_exec(&block) if block_given?
+    @members = []
+    self.class.adjust_level(@level)
+    @level.members.each { |member| member.freeze }
+    @members = self.class.flatten_level(@level)
+    @members.each(&:freeze)
+    @members.freeze
   end
+
+
+  #
+  # :nodoc:
+  #
+  # Flattens a MemberStackLevel's members into a single array and returns an
+  # array of StructMemberInfo objects.
+  #
+  def self.flatten_level(level, info_buffer = [])
+    level.members.each { |m|
+      if m.kind_of?(MemberStackLevel)
+        flatten_level(m, info_buffer)
+      else
+        info_buffer.push(m)
+      end
+    }
+    info_buffer
+  end
+
+
+  #
+  # :nodoc:
+  #
+  # Iterates over a level's members, including sub-levels, and adjusts their
+  # offsets and the level's size accordingly. This is essentially the processing
+  # phase done to give members their offsets and levels their sizes.
+  #
+  def self.adjust_level(level, start_at: 0)
+    base_offset = offset = ::Snow::Memory.align_size(start_at, level.alignment)
+    level.size = 0
+    level.members.each do |m|
+      m.offset = offset = ::Snow::Memory.align_size(offset, m.alignment)
+      if m.kind_of?(MemberStackLevel)
+        adjust_level(m, start_at: offset)
+      end
+      if level.is_union
+        level.size = [level.size, (m.offset + m.size) - base_offset].max
+      else
+        level.size = (m.offset + m.size) - base_offset
+        offset += m.size
+      end
+    end
+  end
+
 
   #
   # call-seq:
   #     member_info => [StructMemberInfo]
   #
-  # Returns the StructMemberInfo array for the builder. You should copy this if
-  # you need to modify it, and the same goes for its elements.
+  # Returns the StructMemberInfo array for the builder.
   #
   def member_info
     @members
   end
+
 
   #
   # call-seq:
@@ -59,7 +129,13 @@ class Snow::CStruct::Builder
       method_name = method_name.to_sym
 
       __send__(:define_method, method_name) do | name, lengths = 1, align: nil |
+        level = instance_variable_get(:@level)
+
+        member_names = instance_variable_get(:@member_names)
+
         name = name.to_sym
+        raise ArgumentError, "#{name} redefined in struct" if member_names.include?(name)
+        member_names.add(name)
 
         length = case lengths
                  when Integer, Fixnum then lengths
@@ -73,15 +149,21 @@ class Snow::CStruct::Builder
         align = (align || ::Snow::CStruct::ALIGNMENTS[type_name]).to_i
         raise "Nil alignment for type #{type_name}" if align.nil?
 
-        last_offset = instance_variable_get(:@last_offset)
-        offset = ::Snow::Memory.align_size(last_offset, align)
-        last_offset = offset + size
+        base_offset = level.offset
+        offset = ::Snow::Memory.align_size(base_offset, align)
+        level.offset = offset + size unless level.is_union
 
         member_info = ::Snow::CStruct::StructMemberInfo[
           name, type_name, size, length, align, offset]
 
-        instance_variable_set(:@last_offset, last_offset)
-        instance_variable_get(:@members).push(member_info.freeze)
+        level.alignment = [level.alignment, align].max
+        if level.is_union
+          level.size = [level.size, size].max
+        else
+          level.size += (offset - base_offset) + size
+        end
+
+        level.members.push(member_info)
       end # define_method(type_name)
 
       @@defined_types.add(type_name)
@@ -94,5 +176,76 @@ class Snow::CStruct::Builder
 
     self
   end # flush_type_methods!
+
+
+  #
+  # :nodoc:
+  #
+  # Creates a new member level for the builder and instance_exec-s the block,
+  # then passes its alignment onto its ancestor level.
+  #
+  def __do_level__(align: nil, is_union: false, &block)
+    parent = @level
+    next_level = MemberStackLevel[!!is_union, 0, 1, 0, []]
+    @level = next_level
+
+    self.instance_exec(&block)
+
+    next_level.alignment = align || next_level.alignment
+    parent.alignment = [parent.alignment, next_level.alignment].max
+
+    @level = parent
+    parent.members.push(next_level)
+  end
+
+
+  #
+  # call-seq:
+  #     union { ... }
+  #     union(align: nil) { ... }
+  #
+  # For the scope of the block, any members declared are considered union
+  # members, as opposed to struct members. Each member of a union occupies the
+  # same space as other members of the union, though their offsets may differ
+  # if their alignments differ as well.
+  #
+  # If no alignment is specified for the union, its base offset will be aligned
+  # to that of the member with the largest alignment. Otherwise, if an alignment
+  # is specified, members may not occupy the same offsets relative to the
+  # beginning of the union.
+  #
+  # For example, if a union with an alignment of 4 has uint32_t and uint64_t
+  # members with default alignments with a starting offset of 4, the uint32_t
+  # member will be located at offset 4, while the uint64_t member will be at
+  # offset 8. As such, it's best to leave union alignments at their default
+  # unless absolutely necessary.
+  #
+  def union(align: nil, &block)
+    __do_level__(align: align, is_union: true, &block)
+  end
+
+
+  #
+  # call-seq:
+  #     struct { ... }
+  #     struct(align: nil) { ... }
+  #
+  # For the scope of the block, any members declared are considered struct
+  # members, as opposed to union members.
+  #
+  # Each member of a struct occupies its own space inside the struct, unlike a
+  # union (where each member occupies either the same or adjacent space in the
+  # union).
+  #
+  # Unless an alignment is specified, the default alignment of a struct is that
+  # of the largest alignment of all its members. If specifying an alignment,
+  # keep in mind that the members of the struct may need to also be manually
+  # aligned, otherwise the first member may be preceeded by padding bytes
+  # regardless of the start of the struct. See #union for more information on
+  # alignment oddities.
+  #
+  def struct(align: nil, &block)
+    __do_level__(align: align, is_union: false, &block)
+  end
 
 end
